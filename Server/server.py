e90a1b068373350cb1a27e9d393c4e3e7d3aecd4
gFagnaniJob <gfagnani.job@gmail.com>
2018-11-29 15:21:07 +0100

    remove useless get api

--- Server/server.py
+++ Server/server.py
@@ -5,25 +5,6 @@ import base64
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
-@app.route('/api/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
 @app.route('/api/test', methods=['POST'])
 def write_image():
     if not request.json or not 'img' in request.json or not 'name' in request.json:

23d8a09919205fa0925e8b5115911aab175553f0
gFagnaniJob <gfagnani.job@gmail.com>
2018-11-29 15:17:46 +0100

    completed send/receive jpg file

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,15 +1,61 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace Client
 {
     class Program
     {
+        private static readonly HttpClient client = new HttpClient();
+        private static int count = 0;
+
         static void Main(string[] args)
         {
-            /*
-            REST CALL:
-            POST /api/image  ->  invio jpg a server che risponde con predictions (json)
-             */
+            Method();
+            Console.ReadKey(true);
+        }
+
+        static async void Method ()
+        {
+            //To get the location the assembly normally resides on disk or the install directory
+            string path = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+
+            //once you have the path you get the directory with:
+            var directory = Path.GetDirectoryName(path).Replace("file:\\", "").Replace("bin\\Debug\\netcoreapp2.0", "images\\");
+
+            Console.WriteLine(directory);
+
+            using (var stream = File.Open(directory + "image.jpg", FileMode.Open))
+            {
+                Byte[] bytes = new byte[(int)stream.Length];
+                stream.Read(bytes, 0, (int)stream.Length);
+
+                var base64Image = Convert.ToBase64String(bytes);
+                string name = "image" + count;
+
+                var dic = new Dictionary<string, string>
+                {
+                    {"img", base64Image },
+                    {"name", name }
+                };
+
+                string json = JsonConvert.SerializeObject(dic);
+
+                var content = new StringContent(json);
+                content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                var response = await client.PostAsync("http://192.168.0.231:5000/api/test", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(response.StatusCode);
+                Console.WriteLine(response);
+                Console.WriteLine(responseString);
+            }
         }
     }
 }
--- Server/server.py
+++ Server/server.py
@@ -1,2 +1,40 @@
-# API
-# POST /api/image  ->  ricevo immagine e invio json con predictions
+from flask import Flask, jsonify
+from flask import request
+from flask import abort
+import base64
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/api/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/api/test', methods=['POST'])
+def write_image():
+    if not request.json or not 'img' in request.json or not 'name' in request.json:
+        abort(400)
+    name = request.json["name"]
+    with open('received_images/' + name + '.jpg', 'wb') as f:
+        imageBytes = request.json["img"]
+        f.write(base64.decodebytes(imageBytes.encode()))
+    f.close()
+    response = "OK"
+    return jsonify({"response": response})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
